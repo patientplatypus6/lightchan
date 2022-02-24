b8dcacd068211afb0207882a1954476fae50d7fe
Peter Weyand <patientplatypus6@gmail.com>
2022-02-24 08:52:21 -0500

    minor update

--- src/pages/components/commentcontainer.js
+++ src/pages/components/commentcontainer.js
@@ -1,10 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import '../main.css'
 
+import img404 from './img404.jpeg'
 
 function CommentContainer({comment, handleNavigate, showthread}){
 
+  const [imgError, setImgError] = useState(false)
   console.log('value of comment.image_path: ', comment.file_name)
+
+  const pichandler=()=>{
+
+    console.log('inside pichandler')
+
+    if(comment.file_name!=""&&comment.file_name!=undefined){
+      if(imgError){
+        return(
+          <img
+            src={img404} 
+            className='commentimage'
+          />
+        )
+      }else{
+        return(
+          <img
+            onError={()=>{
+              console.log("inside the onError function")
+              setImgError(true)
+            }}
+            src={"http://localhost:8000/static/"+comment.file_name} 
+            className='commentimage'
+          />
+        )
+      }
+    }else{
+      <div/>
+    }
+
+  }
 
   return(
     <div className='commentcontainer'>
@@ -15,7 +47,7 @@ function CommentContainer({comment, handleNavigate, showthread}){
       }}>view thread</span>:<span/>}
       </div>
       <div className='commentcontentbox'>
-        {comment.file_name!=""&&comment.file_name!=undefined?<img src={"http://localhost:8000/static/"+comment.file_name} className='commentimage'/>:<div/>}
+        {pichandler()}
         <div className='commentcontent'>
           {comment.content}
         </div>
