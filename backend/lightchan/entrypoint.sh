1b951803aefcfe4bd3a1a6e5cefe724a9c4b1456
Peter Weyand <peterweyand@Peters-MacBook-Pro.local>
2022-03-02 15:53:06 -0500

    docker_issues

--- backend/lightchan/entrypoint.sh
+++ backend/lightchan/entrypoint.sh
@@ -4,4 +4,11 @@
 python manage.py makemigrations
 python manage.py migrate
 
+python manage.py createsuperuser \
+  --noinput \
+  --username pixel \
+  --email peterweyand0@gmail.com
+# --database lightchan
+
+
 
--- backend/lightchan/runpostgres.sh
+++ backend/lightchan/runpostgres.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+		echo "dev-postgres exists creating database table..."
+                echo "drop database if exists lightchan" | docker exec -i dev-postgres psql -U postgres
+		echo "create database lightchan;" | docker exec -i dev-postgres psql -U postgres
+		echo "drop user if exists pixel;" | docker exec -i dev-postgres psql -U postgres
+		echo "create user pixel with encrypted password 'lightchan';" | docker exec -i dev-postgres psql -U postgres
+		echo "grant all privileges on database lightchan to pixel;" | docker exec -i dev-postgres psql -U postgres
+         
+#docker exec -it -u root dev-postgres psql -h localhost -U postgres
+#docker exec -it -u root dev-postgres psql -U postgres
+#docker exec dev-postgres psql <<EOF 
+#\l 
+#EOF
+#docker exec -it -u root dev-postgres postgres psql -h localhost -p 5432
+#docker exec -it -u root dev-postgres postgres -U 
+#docker exec -it -u root dev-postgres psql -h localhost -U postgres
+#docker exec -it -u root dev-postgres create database lightchan;
+#docker exec -it -u root dev-postgres create user pixel with encrypted password 'lightchan';
+#docker exec -it -u root dev-postgres grant all privileges on database lightchan to pixel;
+#docker exec -it -u root dev-postgres \q;
+#docker exec -it -u root dev-postgres exit;
 
-docker pull postgres
-
-docker run -d \
-	--name dev-postgres \
-	-e POSTGRES_USERNAME=pixel \
-	-e POSTGRES_PASSWORD=stardust \
-	-v ${HOME}/postgres-data/:/var/lib/postgresql/data \
-        -p 5432:5432 \
-        postgres
 
